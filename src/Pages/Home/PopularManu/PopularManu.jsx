8a5ee7ff7a525dc9193da0032a9c892411e9f39a
Jahirul Islam <nockjahirul@gmail.com>
2025-07-24 21:50:04 -0700

    add some blog populermanue and menuitems

--- src/Pages/Home/PopularManu/PopularManu.jsx
+++ src/Pages/Home/PopularManu/PopularManu.jsx
@@ -1,28 +1,28 @@
-import React, { useState } from 'react';
+import React, { useLayoutEffect, useState, } from 'react';
 import SectionTitle from '../../../Components/Section/SectionTitle';
 import MenuItem from '../../Shard/MenuItems/MenuItem';
 
+
 const PopularManu = () => {
-  const [manue,setManue]= useState()
+  const [manue,setManue]= useState([]);
   useLayoutEffect(()=>{
-    fetch('manue.json')
+    fetch('Manue.json')
     .then(res =>res.json())
     .then(data=>{
       const popularItems = data.filter(item =>item.category === 'popular');
       setManue(popularItems)
-    })
+    });
   },[])
   return (
-    <section>
+    <section className="mb-12">
        <SectionTitle
        heading="From Our Menu"
        subHeading="Popular Items"
        ></SectionTitle>
-       <div>
+       <div className='grid md:grid-cols-2 gap-10'>
         {
-          menu.map(item=> <MenuItem 
-          key={item._id}>
-          item={item}
+          manue.map(item=> <MenuItem 
+          key={item._id} item={item}>
           </MenuItem>)
         }
        </div>
